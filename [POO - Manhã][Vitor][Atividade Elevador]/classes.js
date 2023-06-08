fe27bfac6f26f21ed067ab9f43ed2ce1182fdd75
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2023-06-08 15:43:26 -0300

    Update classes.js

--- [POO - Manhã][Vitor][Atividade Elevador]/classes.js
+++ [POO - Manhã][Vitor][Atividade Elevador]/classes.js
@@ -1,34 +1,38 @@
-class Passageiro{
-    constructor(andarPretendido){
+class Passageiro {
+    constructor(andarPretendido) {
         this.andarPretendido = andarPretendido
     }
 }
 
-class Elevador{
-    constructor(){
+class Elevador {
+    constructor() {
         this.andarAtual = 0
         this.quantidadeMaximaDePassageiros = 9
         this.listaDePassageiros = []
     }
-    adicionarPassageiro(passageiro){
-        if(this.listaDePassageiros.length < this.quantidadeMaximaDePassageiros){
+    adicionarPassageiro(passageiro) {
+        if (this.listaDePassageiros.length < this.quantidadeMaximaDePassageiros) {
             this.listaDePassageiros.push(passageiro)
             console.log(`1 passageiro que deseja ir no andar ${passageiro.andarPretendido} entrou no elevador. Agora o elevador tem ${this.listaDePassageiros.length} passageiro(s)`);
-        }else {
+        } else {
             console.log(`O passageiro não pode entrar. O elevador já atingiu a capacidade máxima: 
             ${this.quantidadeMaximaDePassageiros}`)
         }
     }
-    movimentar(andarDeDestino){
+
+    movimentar(andarDeDestino) {
         this.andarAtual = andarDeDestino
-        let saida = 0
-        
-        for(let i = 0; i < this.listaDePassageiros.length; i++){
-            if(this.listaDePassageiros[i].andarPretendido == this.andarAtual){
+        let saida = 0 // [2, 2, 2 ,3]
+        for (let i = this.listaDePassageiros.length - 1; i >= 0; i--) {
+            if (this.listaDePassageiros[i].andarPretendido === this.andarAtual) {
                 saida++
+                this.listaDePassageiros.splice(i, 1)
             }
         }
-        console.log(`MOVIMENTAÇÃO \n Agora o elevador está no andar ${this.andarAtual}. ${saida} passageiros SAÍRAM do elevador \n ${this.listaDePassageiros.length} passageiros CONTINUAM do elevador. ${this.listaDePassageiros}`);
+        console.log(` =========================================================== 
+\nMOVIMENTAÇÃO\nAgora o elevador está no andar ${this.andarAtual}. ${saida} passageiros SAÍRAM do elevador 
+\n${this.listaDePassageiros.length} passageiros CONTINUAM do elevador.`); console.log(this.listaDePassageiros)
+
     }
 }
 
