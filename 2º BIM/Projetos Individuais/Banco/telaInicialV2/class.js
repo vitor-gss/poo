13c6642aa65d4d4aed6e88635ccdc425340c9b82
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2023-06-14 19:13:59 -0300

    Adição funções adicionarConta e adicionarUsuario

--- 2º BIM/Projetos Individuais/Banco/telaInicialV2/class.js
+++ 2º BIM/Projetos Individuais/Banco/telaInicialV2/class.js
@@ -13,23 +13,32 @@ class Account { // Informações da conta
     }
 }
 class Bank {
-    constructor(account, user, nameBank) {
+    constructor(account, user) {
         this.account = account;
-        this.users = [{ // armazena as informações dos usuários e contas
-            cpf: user.cpf,
-            name: user.name,
-            password: user.password,
-            type: account.type,
-            saldo: account.saldo,
-            nameBank: nameBank
-        }];
+        this.cpf = user.cpf,
+        this.name = user.name,
+        this.password = user.password,
+        this.type = account.type,
+        this.saldo = account.saldo
+        this.users = []
+        this.account = []
+    }
+    adicionarUsuario(user){
+        this.users.push(user)
+    }
+    adicionarConta(account){
+        this.account.push(account)
     }
 }
 
 var user = new User("Vitão", 2023, "03895124444");
 var account = new Account(user.cpf, "Corrente", 32);
-var bank = new Bank(account, user, "Itaí");
 var user2 = new User("Gabiel", 2022, "03888124443");
 var account2 = new Account(user2.cpf, "Poupança", 986);
-var bank2 = new Bank(account2, user2, "CuBank");
+var bank = new Bank(account, user);
+bank.adicionarConta(account)
+bank.adicionarUsuario(user)
+bank.adicionarConta(account2)
+bank.adicionarUsuario(user2)
+
 // Dúvida -> Como acessar cada usuário?
