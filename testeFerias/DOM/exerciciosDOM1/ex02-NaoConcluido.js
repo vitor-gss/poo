f1bb267260bb5cef50dc972d1ca88b3d0777ca76
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2023-07-11 10:41:12 -0300

    A

--- testeFerias/DOM/exerciciosDOM1/ex02-NaoConcluido.js
+++ testeFerias/DOM/exerciciosDOM1/ex02-NaoConcluido.js
@@ -1,6 +1,6 @@
 const botao = document.querySelector("#btBotao")
 const container = document.querySelector("#container")
 
-botao.addEventListener("click",()=>{
-    
+botao.addEventListener("click", () => {
+    container.removeChild(container.firstChild)
 })
